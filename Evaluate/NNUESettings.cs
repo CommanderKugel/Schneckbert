3d3f5fa416925a6c9e2acd050bc52cf77e6a001f
Nikolas Windheuser <nikolaswindheuser@gmail.com>
2025-03-28 08:54:52 +0100

    new net (768hm -> 768)x2 -> 1x8

--- Evaluate/NNUESettings.cs
+++ Evaluate/NNUESettings.cs
@@ -6,7 +6,7 @@ public static class NNUESettings
     public static readonly int ITERATIONS  = FT_SIZE / VECTOR_SIZE;
 
     public const int INPUT_SIZE  = 768;
-    public const int FT_SIZE     = 512;
+    public const int FT_SIZE     = 768;
     public const int OUTPUT_SIZE = 1;
 
     public const int IN_BUCKET_CNT = 1;
@@ -14,7 +14,7 @@ public static class NNUESettings
     public const int OUT_BUCKET_CNT = 8;
     public static int OUT_BUCK_DIVISOR = (int)Math.Ceiling(32.0d / (double)OUT_BUCKET_CNT);
     
-    public const string net_name = "simple512";
+    public const string net_name = "simple768";
     
     public const int SCALE = 400;
     public const short QA = 255;
